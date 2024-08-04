641cfb3c3beaccfcdd9c3c1f7cd50c05fd03752b
Shivg2901 <guptashivansh374@gmail.com>
2024-08-04 23:34:03 +0530

    init worker

---
+++ worker/src/index.ts
@@ -0,0 +1,41 @@
+import { Kafka } from "kafkajs"
+const TOPIC_NAME = "zap-events"
+
+const kafka = new Kafka({
+    clientId: "outbox-processors",
+    brokers: ['localhost:9092']
+})
+
+async function main() {
+    const consumer = kafka.consumer({
+        groupId: 'main-worker'
+    });
+    await consumer.connect()
+    
+    await consumer.subscribe({
+        topic: TOPIC_NAME,
+        fromBeginning: true
+    })
+
+    await consumer.run({
+        autoCommit: false,
+        eachMessage: async({topic, partition, message}) => {
+            console.log({
+                partition,
+                offset: message.offset,
+                value: message.value?.toString()
+            })
+
+
+            await new Promise(r => setTimeout(r, 5000))
+            await consumer.commitOffsets([{
+                topic: TOPIC_NAME,
+                partition: partition,
+                offset: (parseInt(message.offset) + 1).toString()
+            }])
+
+        }
+    })
+}
+
+main()
