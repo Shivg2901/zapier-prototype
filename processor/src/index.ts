f63af49e2f9cf08fe2818a34bf02d4981a36c189
Shivg2901 <guptashivansh374@gmail.com>
2024-08-04 23:02:02 +0530

    init processor

---
+++ processor/src/index.ts
@@ -0,0 +1,39 @@
+import { PrismaClient } from "@prisma/client";
+import { Kafka } from "kafkajs";
+
+const client = new PrismaClient();
+const TOPIC_NAME = 'zap-events';
+
+const kafka = new Kafka({
+    clientId: 'outbox-processor',
+    brokers: ['localhost:9092']
+  })
+
+async function main() {
+    const producer = kafka.producer();
+    await producer.connect();
+
+    while(1) {
+         const pendingRows = await client.zapRunOutbox.findMany({
+            where: {},
+            take: 10
+        })
+
+        producer.send({
+            topic: TOPIC_NAME,
+            messages: pendingRows.map(r => ({
+                value: r.zapRunId
+            }))
+        })
+
+        await client.zapRunOutbox.deleteMany({
+            where: {
+                id: {
+                    in: pendingRows.map(r => r.id)
+                }
+            }
+        })
+    }
+}
+
+main()
