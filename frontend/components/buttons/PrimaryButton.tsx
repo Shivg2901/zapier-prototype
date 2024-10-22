1780f8ab66acab51fa7e83706943b6498b85b953
KhushiParekh <140279193+KhushiParekh@users.noreply.github.com>
2024-10-22 21:15:39 +0530

    login UI

--- frontend/components/buttons/PrimaryButton.tsx
+++ frontend/components/buttons/PrimaryButton.tsx
@@ -6,7 +6,7 @@ const PrimaryButton = ({children, onClick, size = "small"}: {children: ReactNode
 onClick: () => void, size?: "small" | "big"}) => {
     return <div className={`${size==="small"? "text-sm": "text-xl"}
     ${size==="small"? "px-8 py-2": "px-14 py-3"} bg-amber-700 text-white rounded-full
-    hover:shadow-md cursor-pointer text-center flex flex-col justify-center`} onClick={onClick}>
+    hover:shadow-lg cursor-pointer text-center flex flex-col justify-center hover:bg-[#ec4d03]`} onClick={onClick}>
     {children}
     </div>
 }
